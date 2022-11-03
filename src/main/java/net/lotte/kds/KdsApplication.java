d24f562ace68b2269d069687b8243cb5512d378e
soohyun <soohyun6879@naver.com>
2022-11-03 20:22:05 +0900

    initial commit

---
+++ src/main/java/net/lotte/kds/KdsApplication.java
@@ -0,0 +1,13 @@
+package net.lotte.kds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/lotte/kds/KdsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lotte.kds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
